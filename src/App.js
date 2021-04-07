13380c6390ee012e1e0162b573bac91625e69b3b
Suisuikitai <mag.cupw@gmail.com>
2021-04-07 22:43:40 +0900

    カスタムフック

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [email, setEmail] = useState("");
-  const handleEmail = (e) => setEmail(e.target.value);
+  const email = useInput("");
 
-  const [password, setPassword] = useState("");
-  const handlePassword = (e) => setPassword(e.target.value);
+  const password = useInput("");
 
-  const [name, setName] = useState("");
-  const handleName = (e) => setName(e.target.value);
+  const name = useInput("");
 
-  const [city, setCity] = useState("");
-  const handleCity = (e) => setCity(e.target.value);
+  const city = useInput("");
 
-  const submit = () => alert(`email: ${email}, password: ${password}`);
+  const submit = () => alert(`email: ${email.value}, password: ${password.value}`);
 
   return (
     <div className="App">
       <div className="container">
-        <input placeholder="email" value={email} onChange={handleEmail} />
+        <input placeholder="email" value={email.input} onChange={email.handleInput} />
         <input
           placeholder="password"
-          value={password}
-          onChange={handlePassword}
+          value={password.value}
+          onChange={password.handleInput}
         />
-        <input placeholder="name" value={name} onChange={handleName} />
-        <input placeholder="city" value={city} onChange={handleCity} />
+        <input placeholder="name" value={name.value} onChange={name.handleInput} />
+        <input placeholder="city" value={city.value} onChange={city.handleInput} />
         <button onClick={submit} type="button">
           login
         </button>
@@ -36,10 +32,11 @@ export default function App() {
 }
 
 function useInput(initial){
-  const [input, setInput] = useState(initial);
+  const [value, setValue] = useState(initial);
 
   //返したいものを返して良い(返り値の型とか考えなくて良い)
   //useStateを使用する単なる関数を定義すると捉える
   //カスタムフック === フックを使った便利関数
-
+  const handleInput = e => setValue(e.target.value);
+  return {value, handleInput};
 }
